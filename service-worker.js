601a54271c069328ca20be32d377af124a031371
Serjeo <98581061+SerjeoWN@users.noreply.github.com>
2025-06-21 23:19:26 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('epoxy-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
