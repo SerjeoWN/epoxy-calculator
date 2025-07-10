01f0f138d7b12293fd72727063fc7e3af0d96548
Serjeo <98581061+SerjeoWN@users.noreply.github.com>
2025-07-10 11:45:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('epoxy-cache-v1').then(function(cache) {
+    caches.open('epoxy-cache').then(cache => {
       return cache.addAll([
         './',
         './index.html',
@@ -12,9 +12,9 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
